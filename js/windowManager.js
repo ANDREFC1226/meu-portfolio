c4109c7f67d5facfbf579fab9e006452017b486b
ANDREFC1226 <andreferreiracrn03@gmail.com>
2026-08-17 17:50:27 -0300

    windowManager.js

---
+++ js/windowManager.js
@@ -0,0 +1,27 @@
+export class WindowManager {
+  constructor(windowId, titleId, iframeId, closeBtnId) {
+    this.windowEl = document.getElementById(windowId);
+    this.titleEl = document.getElementById(titleId);
+    this.iframeEl = document.getElementById(iframeId);
+    this.closeBtnEl = document.getElementById(closeBtnId);
+
+    this.initEvents();
+  }
+
+  initEvents() {
+    if (this.closeBtnEl) {
+      this.closeBtnEl.addEventListener('click', () => this.close());
+    }
+  }
+
+  open(title, driveUrl) {
+    if (this.titleEl) this.titleEl.innerText = title;
+    if (this.iframeEl) this.iframeEl.src = driveUrl;
+    if (this.windowEl) this.windowEl.classList.add('open');
+  }
+
+  close() {
+    if (this.windowEl) this.windowEl.classList.remove('open');
+    if (this.iframeEl) this.iframeEl.src = 'about:blank';
+  }
+}
