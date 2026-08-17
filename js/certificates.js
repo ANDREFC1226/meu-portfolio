c0abb8163315c56d52ab63373a606e1ae0a9b9b1
ANDREFC1226 <andreferreiracrn03@gmail.com>
2026-08-17 17:50:11 -0300

    certificates.js

---
+++ js/certificates.js
@@ -0,0 +1,16 @@
+export const certificates = [
+  {
+    id: "folder-drive",
+    title: "Pasta de Certificados (Drive)",
+    category: "Drive",
+    driveUrl: "https://drive.google.com/embeddedfolderview?id=1WrZsCIe-VJZgn92JUsf6WE0hVH7rr997#list",
+    icon: "📁"
+  },
+  {
+    id: "cert-1",
+    title: "Certificado 1",
+    category: "Estudos",
+    driveUrl: "https://drive.google.com/file/d/SEU_ID_AQUI/preview",
+    icon: "📜"
+  }
+];
